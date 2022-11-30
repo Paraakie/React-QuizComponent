986b26e660f761034c09c761910b43cd241af845
Sven <sven.m.gerhards@gmail.com>
2022-11-30 13:42:25 +1300

    Added basic page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import './App.css'
+import QUiz from './Quiz.js'
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <Quiz />
     )
   }
 }
